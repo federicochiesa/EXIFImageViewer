eff150515bd2bad071e2dc3a1a80d29d8fe65500
Federico Chiesa <federico.chiesa@me.com>
2022-02-18 15:41:13 +0100

    add basic window layout and menu bar item

--- EXIF.py
+++ EXIF.py
@@ -7,9 +7,22 @@ class ImageViewerWindow(w.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Image Viewer")
+        extLayout = w.QVBoxLayout()
+        bottomButtonsLayout = w.QHBoxLayout()
+        extLayout.addLayout(bottomButtonsLayout)
+        toolbar = w.QToolBar()
+        self.addToolBar(toolbar)
+        self.setStatusBar(w.QStatusBar(self))
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction("test")
+
 
 
 application = w.QApplication([])
+application.setApplicationName("Test")
+
 imgviewindow = ImageViewerWindow()
 imgviewindow.show()
 application.exec()
