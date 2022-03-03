699a372117eb55862cf5ed704ef48b2c3ed9ff7c
Federico Chiesa <federico.chiesa@me.com>
2022-03-03 13:03:21 +0100

    add location map action and window, bug fixes

--- EXIF.py
+++ EXIF.py
@@ -1,10 +1,11 @@
 import PyQt5.QtWidgets as w
 import PyQt5.QtGui as g
 import PyQt5.QtCore as c
+import PyQt5.QtWebEngineWidgets as web
 import exifread
 import sys
 
-#TODO: add gmaps imtegration
+#TODO: add max scaling of 512px
 
 class ImageViewerWindow(w.QMainWindow):
     def __init__(self):
@@ -58,31 +59,35 @@ class ImageViewerWindow(w.QMainWindow):
         self.zoomOutAction.setShortcut(g.QKeySequence.ZoomOut)
         self.zoomOutAction.triggered.connect(lambda: self.scaleImage(zoomIn = False))
 
+        self.locationAction = w.QAction("Show Image Location", self)
+        self.locationAction.setStatusTip("Show the image location on Google Maps. (opens in a browser window)")
+        self.locationAction.setShortcut(g.QKeySequence(c.Qt.CTRL + c.Qt.Key_L))
+        self.locationAction.triggered.connect(self.showLocation)
+
         # Toolbar elements
         toolbar = w.QToolBar("Top toolbar")
         toolbar.setMovable(False)
         toolbar.setContextMenuPolicy(c.Qt.PreventContextMenu)
         self.addToolBar(toolbar)
-        # Status bar elements
+        # Status bar declaration
         self.setStatusBar(w.QStatusBar(self))
         # Menu bar elements
         menu = self.menuBar()
         fileMenu = menu.addMenu("&File")
         editMenu = menu.addMenu("&Edit")
-        editMenu.addAction(self.RCWAction)
-        editMenu.addAction(self.RCCWAction)
-        editMenu.addAction(self.zoomInAction)
-        editMenu.addAction(self.zoomOutAction)
-        toolbar.addAction(self.RCWAction)
-        toolbar.addAction(self.RCCWAction)
-        toolbar.addAction(self.zoomInAction)
-        toolbar.addAction(self.zoomOutAction)
-        # Add actions to toolbar and menu
+        # Add actions to toolbar and edit menu
+        for element in (toolbar, editMenu):
+            element.addAction(self.RCWAction)
+            element.addAction(self.RCCWAction)
+            element.addAction(self.zoomInAction)
+            element.addAction(self.zoomOutAction)
+        # Add actions to toolbar and file menu
         for element in (toolbar, fileMenu):
             element.addAction(self.openAction)
             element.addAction(self.previousAction)
             element.addAction(self.nextAction)
             element.addAction(self.EXIFAction)
+            element.addAction(self.locationAction)
     
     def changeImage(self, next):
         if next:
@@ -98,12 +103,12 @@ class ImageViewerWindow(w.QMainWindow):
         self.label.setPixmap(g.QPixmap(self.loadedImagePaths[self.imageIndex]).transformed(g.QTransform().rotate(self.angle), c.Qt.SmoothTransformation))
         self.label.adjustSize()
     
-    def scaleImage(self, zoomIn): #TODO: finish implementation
+    def scaleImage(self, zoomIn):
         if zoomIn:
             self.imageWidth *= 1.25
         else:
             self.imageWidth /= 1.25
-        self.label.setPixmap(g.QPixmap(self.loadedImagePaths[self.imageIndex]).scaledToWidth(self.imageWidth))
+        self.label.setPixmap(g.QPixmap(self.loadedImagePaths[self.imageIndex]).scaledToWidth(int(self.imageWidth)))
 
     def showImageAtIndex(self, index):
         image = g.QPixmap(self.loadedImagePaths[index])
@@ -114,6 +119,11 @@ class ImageViewerWindow(w.QMainWindow):
         self.label.adjustSize()
         self.resize(self.label.sizeHint())
         self.imageWidth = image.rect().width()
+        self.model = EXIFModel(self.loadedImagePaths[index])
+        if self.model.getEXIFLocation() != (None, None):
+            self.locationAction.setEnabled(True)
+        else:
+            self.locationAction.setEnabled(False)
 
     def openMenuDialog(self, firstStart = False):
         self.loadedImagePaths, _ = w.QFileDialog.getOpenFileNames(parent=self, caption="Select one or more JPEG files to open:", filter="JPEG Image(*.jpg *.jpeg)")
@@ -132,34 +142,46 @@ class ImageViewerWindow(w.QMainWindow):
             sys.exit()
     
     def showEXIFWindow(self):
-        self.exif = EXIFWindow(self.loadedImagePaths[self.imageIndex])
+        self.exif = EXIFWindow(self.model)
         self.exif.windowClosed.connect(lambda: self.EXIFAction.setEnabled(True))
         self.exif.show()
         self.EXIFAction.setEnabled(False)
 
+    def showLocation(self): # TODO: URL generation based on location
+        self.location = LocationWindow("example.com")
+        self.location.windowClosed.connect(lambda: self.locationAction.setEnabled(True))
+        self.location.show()
+        self.locationAction.setEnabled(False)
+
 class EXIFModel(c.QAbstractTableModel):
     def __init__(self, path):
         super(EXIFModel, self).__init__()
-        exif = self.getEXIFData(path)
+        self.exif = self.getEXIFData(path)
         self.exifData = []
-        for key, value in exif.items():
+        for key, value in self.exif.items():
             self.exifData.append((key, value))
 
     def data(self, index, role):
         if role == c.Qt.DisplayRole:
             key, value = self.exifData[index.row()]
-            print(key, value)
             if index.column() == 0:
                 return key
             elif index.column() == 1:
                 return str(value)
             else:
                 return None
+    
+    def headerData(self, section, orientation, role):
+        if role == c.Qt.DisplayRole:
+            if orientation == c.Qt.Horizontal:
+                return ["Tag ", "Value"][section]
+            if orientation == c.Qt.Vertical:
+                return None
 
-    def rowCount(self, parent = None):
+    def rowCount(self, _ = None):
         return len(self.exifData)
 
-    def columnCount(self, parent = None):
+    def columnCount(self, _ = None):
         return 2
 
     def getEXIFData(self, path):
@@ -179,14 +201,14 @@ class EXIFModel(c.QAbstractTableModel):
 
         return d + (m / 60.0) + (s / 3600.0)
         
-    def getEXIFLocation(self, EXIFData):
+    def getEXIFLocation(self):
         latitude = None
         longitude = None
 
-        GPSLatitude = self.getIfExist(EXIFData, 'GPS GPSLatitude')
-        GPSLatitudeRef = self.getIfExist(EXIFData, 'GPS GPSLatitudeRef')
-        GPSLongitude = self.getIfExist(EXIFData, 'GPS GPSLongitude')
-        GPSLongitudeRef = self.getIfExist(EXIFData, 'GPS GPSLongitudeRef')
+        GPSLatitude = self.getIfExist(self.exif, 'GPS GPSLatitude')
+        GPSLatitudeRef = self.getIfExist(self.exif, 'GPS GPSLatitudeRef')
+        GPSLongitude = self.getIfExist(self.exif, 'GPS GPSLongitude')
+        GPSLongitudeRef = self.getIfExist(self.exif, 'GPS GPSLongitudeRef')
 
         if GPSLatitude and GPSLatitudeRef and GPSLongitude and GPSLongitudeRef:
             latitude = self.convertToDegrees(GPSLatitude)
@@ -202,23 +224,38 @@ class EXIFModel(c.QAbstractTableModel):
 class EXIFWindow(w.QMainWindow):
     windowClosed = c.pyqtSignal()
 
-    def __init__(self, imagePath):
+    def __init__(self, exifModel):
         super().__init__()
         self.setWindowTitle("EXIF Data")
-        self.model = EXIFModel(imagePath)
+        self.model = exifModel
         self.tableView = w.QTableView()
         self.tableView.setModel(self.model)
+        self.tableView.verticalHeader().hide()
         self.setCentralWidget(self.tableView)
         self.tableView.resizeColumnToContents(0)
         self.tableView.resizeColumnToContents(1)
         self.tableView.setFixedWidth(self.tableView.columnWidth(0) + self.tableView.columnWidth(1))
+        self.setMinimumSize(0,600)
+        self.adjustSize()
+    
+    def closeEvent(self, event):
+        self.windowClosed.emit()
+        event.accept()
+
+class LocationWindow(w.QMainWindow):
+    def __init__(self, URL):
+        super(LocationWindow, self).__init__()
+        self.browser = w.QWebEngineView()
+        self.browser.setUrl(c.QUrl(URL))
+        self.setCentralWidget(self.browser)
     
     def closeEvent(self, event):
         self.windowClosed.emit()
         event.accept()
     
-a = w.QApplication([])
-a.setApplicationName("EXIF Image Viewer")
-ivw = ImageViewerWindow()
-ivw.openMenuDialog(firstStart = True)
-a.exec()
+if __name__ == "__main__":
+    a = w.QApplication([])
+    a.setApplicationName("EXIF Image Viewer")
+    ivw = ImageViewerWindow()
+    ivw.openMenuDialog(firstStart = True)
+    a.exec()
